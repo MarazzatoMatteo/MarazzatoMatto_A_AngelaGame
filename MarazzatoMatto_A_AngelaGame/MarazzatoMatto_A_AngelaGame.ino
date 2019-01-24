7fc7fda19298e5a337eae739e4deff5355883718
MarazzatoMatteo <matteo.marazzato@barsanti.edu.it>
2019-01-24 22:09:22 +0100

    24/01/19 Sviluppo Progetto
    Risoluzione errore duplice riga vuota dopo "Turno 2"

--- MarazzatoMatto_A_AngelaGame/MarazzatoMatto_A_AngelaGame.ino
+++ MarazzatoMatto_A_AngelaGame/MarazzatoMatto_A_AngelaGame.ino
@@ -1,4 +1,5 @@
 //PROGETTO MARAZZATO MATTEO
+int comando;
 
 void setup() 
 {
@@ -25,21 +26,26 @@ void loop()
     unavolta = false;
   }*/
 
-  /*Serial.println("TURNO 1");
-  while (Serial.available() == 0){}
-  comando = Serial.parseInt();
+  Serial.println("TURNO 1");
+  while (Serial.available() == 0){} //indica quanti byte sono presenti nella casella di inserimento del monitor seriale (attraverso la codifica ASCII, null=0, 0=48, ...)
+  comando = Serial.parseInt(); //o Serial.read();
+  Serial.println(comando);
 
-  delay(2000);
+  delay(1000);
 
   Serial.println("TURNO 2");
   while (Serial.available() == 0){}
-  comando = Serial.parseInt();*/
+  comando = Serial.parseInt();
+  Serial.println(comando);
 
-  while (Serial.available() == 0);
+  delay(1000);
+
+  /*while (Serial.available() == 0);
+  delay(100);
 
   int val = Serial.read();
 
-  Serial.println(val);
+  Serial.println(val);*/
   
   
   
