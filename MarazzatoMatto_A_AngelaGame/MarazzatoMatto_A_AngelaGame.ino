328f018982d5ad7c4bc0dffd065e4ab0f65c46a8
MarazzatoMatteo <matteo.marazzato@barsanti.edu.it>
2019-01-31 20:13:23 +0100

    31/01/19 Termine sviluppo progetto Angela Game
    Progetto completo al 100% (solo parte di codice)

--- MarazzatoMatto_A_AngelaGame/MarazzatoMatto_A_AngelaGame.ino
+++ MarazzatoMatto_A_AngelaGame/MarazzatoMatto_A_AngelaGame.ino
@@ -14,8 +14,10 @@ void setup()
   Serial.begin(9600);
   nummeta = 0;
   sommamosse = 0;
-  giocvincitore = 0;
-  numturno = 1;
+  giocvincitore = 2;
+  //mossa1 = -1;
+  //mossa2 = -1;
+  numturno = 0;
   Serial.println("INIZIO PARTITA");
 }
 
@@ -26,14 +28,20 @@ void loop()
   while (start == true)
   {
     inserisciGiocata();
-    Serial.println("TURNO " + String(numturno));
+      delay(1000);
+  }
+
+  if (sommamosse != nummeta && sommamosse < nummeta)
+  {
+    giocatore1();
       delay(1000);
   }
   
-  giocatore1();
-    delay(1000);
-  giocatore2();
-    delay(1000);
+  if (sommamosse != nummeta && sommamosse < nummeta)
+  {
+    giocatore2();
+      delay(1000);
+  }
 
   vittoria();
     delay(1000);
@@ -42,22 +50,54 @@ void loop()
 int giocatore1()
 {
   Serial.println("GIOCATORE 1");
-  while (Serial.available() == 0){} //indica quanti byte sono presenti nella casella di inserimento del monitor seriale (attraverso la codifica ASCII, null=0, 0=48, ...)
-  mossa1 = Serial.parseInt(); //o Serial.read();
-  if (numturno == 1 
-  sommamosse = sommamosse + mossa1;
-  Serial.println(sommamosse);
-  giocvincitore = 1;
+  while (giocvincitore == 2)
+  {
+    while (Serial.available() == 0){} //indica quanti byte sono presenti nella casella di inserimento del monitor seriale (attraverso la codifica ASCII, null=0, 0=48, ...)
+    mossa1 = Serial.parseInt(); //o Serial.read();
+    if (numturno == 1 && mossa1 >= 0 && mossa1 < 7)
+    {
+      sommamosse = sommamosse + mossa1;
+      Serial.println("La somma totale è: " + String(sommamosse));
+      giocvincitore = 1;
+    }
+
+    else if (mossa1 != mossa2 && mossa1 > 0 && mossa1 < 7 && mossa1 != (7 - mossa2))
+    {
+      sommamosse = sommamosse + mossa1;
+      Serial.println("Mossa precedente: " + String(mossa2));
+      delay(500);
+      Serial.println("La somma totale è: " + String(sommamosse));
+      giocvincitore = 1;
+    }
+
+    else 
+    {
+      Serial.println("Mossa non valida, inserisci un altro valore");
+    }
+  }
 }
 
 int giocatore2()
 {
   Serial.println("GIOCATORE 2");
-  while (Serial.available() == 0){}
-  mossa2 = Serial.parseInt();
-  sommamosse = sommamosse + mossa2;
-  Serial.println(sommamosse);
-  giocvincitore = 2;
+  while (giocvincitore == 1)
+  {
+    while (Serial.available() == 0){}
+    mossa2 = Serial.parseInt();
+    if (mossa2 != mossa1 && mossa2 >= 0 && mossa2 < 7 && mossa2 != (7 - mossa1))
+    {
+      sommamosse = sommamosse + mossa2;
+      Serial.println("Mossa precedente: " + String(mossa1));
+      delay(500);
+      Serial.println("La somma totale è: " + String(sommamosse));
+      giocvincitore = 2;
+    }
+
+    else 
+    {
+      Serial.println("Mossa non valida, inserisci un altro valore");
+    } 
+  }
 }
 
 void inserisciGiocata()
@@ -76,6 +116,9 @@ void inserisciGiocata()
   {
     start = false;
     Serial.println("Il numero da raggiungere è: " + String(nummeta));
+      delay(500);
+      numturno++;
+    Serial.println("TURNO " + String(numturno));
   }
   //itoa(nummeta,convertitore, 10); //permette di convertire un numero in stringa itoa(n,c,b) n=numero da convertire - c=grandezza array o buffer (maggiore del numero di cifre del numero) - b=base in cui è scritto il numero (2 binaria, 10 decimale, 16 esadecimale)
   delay(1000);
@@ -116,8 +159,10 @@ void vittoria()
 void reset()
 {
   sommamosse = 0;
-  giocvincitore = 0;
+  giocvincitore = 2;
   numturno = 1;
   nummeta = 0;
+  //mossa1 = -1;
+  //mossa2 = -1;
   start = true;
 }
